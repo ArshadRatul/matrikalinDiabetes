58c8aeb81742b6b2103933a8673e91f8814f7df4
ArshadRatul <ratularshad@gmail.com>
2024-06-07 02:02:02 +0600

    added water intake

--- lib/features/authentication/screens/foodIntake/foodIntake.dart
+++ lib/features/authentication/screens/foodIntake/foodIntake.dart
@@ -23,119 +23,141 @@ class _FoodIntakeState extends State<FoodIntake> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: Appbar(),
-      body:
-      SingleChildScrollView(
+      body: SingleChildScrollView(
         child: Column(
-            children: [
-              ///The back button and the feature name
-              BackAndFeature(FeatureName: "আজকের খাবার তথ্য",),
+          children: [
+            ///The back button and the feature name
+            BackAndFeature(
+              FeatureName: "আজকের খাবার তথ্য",
+            ),
 
-              ///Two buttons shows detailed information
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center, // Center the buttons
-                children: [
-                  // Rounded rectangle button
-                  ///Button for today's detailed information
-                  ElevatedButton( // Alternatively use TextButton for no elevation
-                    onPressed: () => print('Button Pressed!'),
-                    child: Text("আজকের বিস্তারিত প্রতিবেদন",),
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: Colors.black,
-                      backgroundColor: TColors.primary,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20.0), // Adjust corner radius
-                      ),
+            ///Two buttons shows detailed information
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center, // Center the buttons
+              children: [
+                // Rounded rectangle button
+                ///Button for today's detailed information
+                ElevatedButton(
+                  // Alternatively use TextButton for no elevation
+                  onPressed: () => print('Button Pressed!'),
+                  child: Text(
+                    "আজকের বিস্তারিত প্রতিবেদন",
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.black,
+                    backgroundColor: TColors.primary,
+                    shape: RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.circular(20.0), // Adjust corner radius
                     ),
                   ),
-                  SizedBox(width: 10.0), // Add some horizontal spacing between buttons
-                  // Icon button
+                ),
+                SizedBox(width: 10.0),
+                // Add some horizontal spacing between buttons
+                // Icon button
 
-                  /// Button to show selected date data
-                  IconButton(
-                    icon: Icon(Icons.calendar_month_rounded),
-                    iconSize: THelperFunction.getScreenHeight()*0.05,// Replace with your desired icon
-                    onPressed: () => print('Icon Button Pressed!'),
+                /// Button to show selected date data
+                IconButton(
+                  icon: Icon(Icons.calendar_month_rounded),
+                  iconSize: THelperFunction.getScreenHeight() * 0.05,
+                  // Replace with your desired icon
+                  onPressed: () => print('Icon Button Pressed!'),
+                ),
+              ],
+            ),
+
+            SizedBox(
+              height: TSize.spaceBtwItems,
+            ),
+
+            ///Graph and water glass
+            Row(
+              children: [
+                ///Graph
+                Column(
+                  children: [
+                    Text(
+                      "আজকের ক্যালরি গ্রহণ",
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.3,
+                      width: MediaQuery.of(context).size.width * 0.6,
+                      child: DailyDataChart(),
+                    ),
+                  ],
+                ),
+
+                ///Glass
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.3,
+                  width: MediaQuery.of(context).size.width * 0.4,
+                  child: AnimatedGlass(
+                      fillPercentage: 0.60), // Set your fill percentage here
+                ),
+              ],
+            ),
+
+            SizedBox(
+              height: TSize.spaceBtwItems,
+            ),
+
+            ///Suggestion box
+            suggestionbox(),
+
+            SizedBox(
+              height: TSize.spaceBtwItems,
+            ),
+
+            ///Bottom Two buttons
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  // Alternatively use TextButton for no elevation
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => CalorieIntake()));
+                  },
+                  child: Text(
+                    "খাদ্য তথ্য দিন ",
                   ),
-                ],
-              ),
-
-              SizedBox(height: TSize.spaceBtwItems,),
-              ///Graph and water glass
-              Row(
-                children: [
-                  ///Graph
-                  Column(
-                    children: [
-                      Text("আজকের ক্যালরি গ্রহণ",),
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height * 0.3,
-                        width: MediaQuery.of(context).size.width * 0.6,
-                        child: DailyDataChart(),
-                      ),
-                    ],
-                  ),
-                  ///Glass
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.3,
-                    width: MediaQuery.of(context).size.width * 0.4,
-                    child: AnimatedGlass(fillPercentage: 0.60), // Set your fill percentage here
-                  ),
-                ],
-              ),
-
-              SizedBox(height: TSize.spaceBtwItems,),
-
-              ///Suggestion box
-              suggestionbox(),
-
-              SizedBox(height: TSize.spaceBtwItems,),
-              ///Bottom Two buttons
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton( // Alternatively use TextButton for no elevation
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => CalorieIntake())
-                      );
-                    },
-                    child: Text("খাদ্য তথ্য দিন ",),
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: Colors.black,
-                      backgroundColor: TColors.primary,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20.0), // Adjust corner radius
-                      ),
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.black,
+                    backgroundColor: TColors.primary,
+                    shape: RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.circular(20.0), // Adjust corner radius
                     ),
                   ),
-
-                  SizedBox(width: TSize.spaceBtwItems,),
-
-
-                  ElevatedButton( // Alternatively use TextButton for no elevation
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => WaterIntake())
-                      );
-                    },
-                    child: Text("পানি পানের তথ্য দিন ",),
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: Colors.black,
-                      backgroundColor: TColors.primary,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20.0), // Adjust corner radius
-                      ),
+                ),
+                SizedBox(
+                  width: TSize.spaceBtwItems,
+                ),
+                ElevatedButton(
+                  // Alternatively use TextButton for no elevation
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => WaterIntake()));
+                  },
+                  child: Text(
+                    "পানি পানের তথ্য দিন ",
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.black,
+                    backgroundColor: TColors.primary,
+                    shape: RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.circular(20.0), // Adjust corner radius
                     ),
                   ),
-                ],
-              )
-            ],
+                ),
+              ],
+            )
+          ],
         ),
-
       ),
     );
   }
 }
-
--- lib/features/authentication/screens/foodIntake/waterIntake/waterIntake.dart
+++ lib/features/authentication/screens/foodIntake/waterIntake/waterIntake.dart
@@ -1,3 +1,5 @@
+import 'package:app/utils/constants/colors.dart';
+import 'package:app/utils/helpers/helper.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../../common/wigets/appbar.dart';
@@ -11,19 +13,119 @@ class WaterIntake extends StatefulWidget {
 }
 
 class _WaterIntakeState extends State<WaterIntake> {
+  List<Map<String, dynamic>> data = [
+    {'সময় ': 1, 'name': 'John Doe'},
+    {'সময় ': 2, 'name': 'Jane Smith'},
+    {'সময় ': 3, 'name': 'Mike Johnson'},
+    {'সময় ': 4, 'name': 'Sarah Brown'},
+    {'সময় ': 5, 'name': 'Chris Lee'},
+    {'সময় ': 6, 'name': 'Patricia Miller'},
+    {'সময় ': 7, 'name': 'James Wilson'},
+  ];
+  void _deleteRow(int id) {
+    setState(() {
+      data.removeWhere((element) => element['সময় '] == id);
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: Appbar(),
-      body:
-      SingleChildScrollView(
-      child: Column(
-      children: [
-      ///The back button and the feature name
-      BackAndFeature(FeatureName: "পানি পানের তথ্য",),
-            ]
-          )
+        appBar: Appbar(),
+        body:
+        SingleChildScrollView(
+            child: Column(
+              children: [
+
+              ///The back button and the feature name
+              BackAndFeature(FeatureName: "পানি পানের তথ্য",),
+
+                ///Table
+
+                centerTable(),
+
+                ///Buttons
+                Column(
+                  children: [
+
+
+                  ],
+                )
+              ]
+            )
         )
-      );
+    );
+  }
+
+  Center centerTable() {
+    return Center(
+                child: Container(
+
+                  color: TColors.primary,
+                  height: THelperFunction.getScreenHeight() * 0.5,
+                  width: THelperFunction.getScreenWidth() * 0.9,
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.vertical,
+                    child: Table(
+                      border: TableBorder.all(width: 1.0, color: Colors.black),
+                      columnWidths: const <int, TableColumnWidth>{
+                        0: FlexColumnWidth(2),
+                        1: FlexColumnWidth(5),
+                        2: FlexColumnWidth(3),
+                      },
+                      children: [
+                        TableRow(
+                          children: [
+                            Container(
+                              color: TColors.secondary,
+                              child: TableCell(
+                                  child: Padding(
+                                      padding: EdgeInsets.all(8.0),
+                                      child: Text('সময় ',textAlign: TextAlign.center,
+                                          style: TextStyle(fontWeight: FontWeight.bold,)))),
+                            ),
+                            Container(
+                              color: TColors.secondary,
+                              child: TableCell(
+                                  child: Padding(
+                                      padding: EdgeInsets.all(8.0),
+                                      child: Text('কত গ্লাস পানি খেয়েছেন',textAlign: TextAlign.center,
+                                          style: TextStyle(fontWeight: FontWeight.bold)))),
+                            ),
+                            Container(
+                              color: TColors.secondary,
+                              child: TableCell(
+                                  child: Padding(
+                                      padding: EdgeInsets.all(8.0),
+                                      child: Text('মুছে ফেলুন', textAlign: TextAlign.center,
+                                          style: TextStyle(fontWeight: FontWeight.bold)))),
+                            ),
+                          ],
+                        ),
+                        for (var row in data)
+                          TableRow(
+                            children: [
+                              TableCell(
+                                  child: Padding(
+                                      padding: EdgeInsets.all(8.0),
+                                      child: Text(row['সময় '].toString()))),
+                              TableCell(
+                                  child: Padding(
+                                      padding: EdgeInsets.all(8.0),
+                                      child: Text(row['name']))),
+                              TableCell(
+                                child: IconButton(
+                                  icon: Icon(Icons.delete),
+                                  onPressed: () {
+                                    _deleteRow(row['সময় ']);
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
+                      ],
+                    ),
+                  ),
+                ),
+              );
   }
 }
